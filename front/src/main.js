4417702604deb4bae4d75a1fcd32e5ced7ac60d6
Paulo Silva <pauloecomercial367@gmail.com>
2024-12-21 18:58:41 -0300

    :sparkles: base front libraries

--- front/src/main.js
+++ front/src/main.js
@@ -1,5 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
+import toastrPlugin from '@/services/toastr';
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+    .use(toastrPlugin)
+    .use(router)
+    .mount('#app')
