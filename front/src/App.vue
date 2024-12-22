d0988b132d491446cff5311f90a38d0846c5cb1d
Paulo Silva <pauloecomercial367@gmail.com>
2024-12-22 20:06:08 -0300

    :gem: basic navbar

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,3 +1,28 @@
 <template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="/support">Chamados App</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="/support"> Home </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link btn btn-sm btn-outline-danger" href="/"> Sair</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
   <router-view />
 </template>
