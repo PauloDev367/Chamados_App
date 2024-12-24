daa1acbf942c2eea35e738d975241069c0504b7a
Paulo Silva <pauloecomercial367@gmail.com>
2024-12-24 16:34:37 -0300

    :sparkles: pagination to closed support requests

--- front/src/components/client/CloseSupportRequestComponent.vue
+++ front/src/components/client/CloseSupportRequestComponent.vue
@@ -1,25 +1,33 @@
 <template>
   <ul class="list-group" v-if="supportRequestsSearch != null">
-    <li class="list-group-item">
-      <template v-for="data in supportRequestsSearch.data" :key="data.id">
-        <ClientFinishedSupportRequestComponent :supportRequest="data" />
-      </template>
-    </li>
+    <template v-for="data in supportRequestsSearch.data" :key="data.id">
+      <ClientFinishedSupportRequestComponent :supportRequest="data" />
+    </template>
   </ul>
+   <div class="mt-4">
+    <Bootstrap4Pagination
+      v-if="supportRequestsSearch != null"
+      :data="supportRequestsSearch"
+      @pagination-change-page="loadData"
+    >
+    </Bootstrap4Pagination>
+  </div>
 </template>
 <script setup>
 import { getClientFinishedSupportRequests } from "@/services/client";
 import { useToastr } from "@/services/toastr";
 import { onMounted, ref } from "vue";
 import ClientFinishedSupportRequestComponent from "@/components/client/ClientFinishedSupportRequestComponent.vue";
+import { Bootstrap4Pagination } from "laravel-vue-pagination";
+
 const supportRequestsSearch = ref(null);
 onMounted(() => {
-  getAllClientSupportRequest(1);
+  loadData(1);
 });
 
 const toastr = useToastr();
 
-const getAllClientSupportRequest = (page) => {
+const loadData = (page) => {
   getClientFinishedSupportRequests(page)
     .then((result) => {
       supportRequestsSearch.value = result.data.success;
